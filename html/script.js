bf62159503f27e42acb6c265ef8557311807d0c9
Yummy_Bacon5 <68166338+YummyBacon5@users.noreply.github.com>
2024-10-23 16:17:05 +0000

    Change indents to tabs

--- html/script.js
+++ html/script.js
@@ -3,16 +3,16 @@ navigator.serviceWorker.register("service-worker.js");
 const image = document.querySelector("img[src=currentImage\\.png]");
 
 navigator.serviceWorker.addEventListener("message", async (e) => {
-    if(!e.data.imageData) return;
-    const tags = await ExifReader.load(e.data.imageData);
-    
-    const altText = tags["alt_text"]?.description;
+	if(!e.data.imageData) return;
+	const tags = await ExifReader.load(e.data.imageData);
 
-    if(!altText) {
-        console.error("No alt text found in image, using default alt text.");
-        return;
-    }
-    
-    image.alt = altText;
-    image.title = altText;
+	const altText = tags["alt_text"]?.description;
+
+	if(!altText) {
+		console.error("No alt text found in image, using default alt text.");
+		return;
+	}
+
+	image.alt = altText;
+	image.title = altText;
 });
